f300748657c3eaf94b865fbdd3595c1924960dda
DynastyElvis <kipkemoi.rono@student.moringaschool.com>
2022-06-11 22:35:50 +0300

    ft: app  updates

--- instagramlite/instagramu/views.py
+++ instagramlite/instagramu/views.py
@@ -45,3 +45,71 @@ def add_user_profile(request):
     else:
         form = NewProfileForm()
     return render(request, 'profile/new_user_profile.html', {"form": form})
+
+
+
+@login_required  
+def upload_image(request):
+    current_user = request.user
+    profiles = Profile.get_profile()
+    for profile in profiles:
+        if profile.user.id == current_user.id:
+            if request.method == 'POST':
+                form = UploadForm(request.POST,request.FILES)
+                if form.is_valid():
+                    upload = form.save(commit=False)
+                    upload.posted_by = current_user
+                    upload.profile = profile
+                    upload.save()
+                    return redirect('homepage')
+            else:
+                form = UploadForm()
+            return render(request,'insta/upload.html',{"user":current_user,"form":form})
+
+def login_user(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+         login(request, user)
+         return redirect('homepage')
+        
+        else:
+            messages.success(request,('Invalid information'))
+            return redirect('login')
+         
+    else:
+
+     return render(request,'registration/login.html')
+
+def register_user(request):
+    if request.method == 'POST':
+         form = UserRegisterForm(request.POST)
+         if form.is_valid():
+             form.save()
+             username = form.cleaned_data['username']
+             password = form.cleaned_data['password1']
+             email = form.cleaned_data['email']
+             send_welcome_email(username,email) 
+
+             user = authenticate(username=username, password=password)
+             login(request,user)
+
+             messages.success(request,f'Hello {username}, Your account was Successfully Created.You will receive our email shortly.Thank You!!!')
+             return redirect('add_profile')
+    else:
+         form = UserRegisterForm()
+    return render (request,'registration/register.html',{'form':form})
+
+@login_required
+def logout_user(request):
+    logout(request)
+    return HttpResponseRedirect(reverse("login"))
+
+@login_required
+def images(request, pk):
+    profile = Profile.objects.get(pk=pk)
+    Images = Image.objects.all().filter(name=pk)
+
+    return render(request, 'insta/images.html',{"profile": profile, 'Images': Images} )
