f6453b334f5a3f9e76def3b0691266ccc6c59d4e
DynastyElvis <kipkemoi.rono@student.moringaschool.com>
2022-06-11 22:43:19 +0300

    ft: app configurations s

--- instagramlite/instagramlite/settings.py
+++ instagramlite/instagramlite/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+from pathlib import Path
+import dj_database_url
+import django_on_heroku
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -31,12 +36,17 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'crispy_forms',
+    'tinymce',
+    'instagramu',
+    'bootstrap4',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'follows',
 ]
 
 MIDDLEWARE = [
