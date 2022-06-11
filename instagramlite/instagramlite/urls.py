f9ab5ab346c6b23489aa8a33537dca9a1a4cef01
DynastyElvis <kipkemoi.rono@student.moringaschool.com>
2022-06-11 22:28:30 +0300

    ft: url patterns

--- instagramlite/instagramlite/urls.py
+++ instagramlite/instagramlite/urls.py
@@ -14,8 +14,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
+from instagramu import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('instagramu.urls')),
+    path('register/',views.register_user,name='register'),
+    path('accounts/login/',views.login_user,name='login'),
+    path('logout/', views.logout_user, name='logout'),
+    path('tinymce/', include('tinymce.urls')),
+   
 ]
+admin.site.site_header= "Instagram Lite Administration"
+admin.site.site_title="Instagram"
+admin.site.index_title="Welcome to Instagram administration"
