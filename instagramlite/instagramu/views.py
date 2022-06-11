a0407842948c7fc7a9e282c882f3208d86b4c368
DynastyElvis <kipkemoi.rono@student.moringaschool.com>
2022-06-11 22:34:54 +0300

    ft: added homepage and login logic

--- instagramlite/instagramu/views.py
+++ instagramlite/instagramu/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+
 
 # Create your views here.
+@login_required
+def homepage(request):
+    Images = Image.get_images()
+    comments = Comment.get_comment()
+    profile = Profile.get_profile()
+    current_user = request.user
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.user = current_user
+            comment.save()
+        return redirect('homepage')
+
+    else:
+        form = CommentForm()
+
+    return render(request,"insta/homepage.html",{"Images":Images, "comments":comments,"form": form,"profile":profile})
+
+@login_required
+def user_profile(request,profile_id):
+    try:
+     profile = Profile.objects.get(pk=profile_id)
+
+    except Profile.DoesNotExist:
+      profile = None
+    Images = Image.objects.filter(profile_id=profile).all()
+
+    return render(request,"profile/profile.html",{"profile":profile,"Images":Images})
+
