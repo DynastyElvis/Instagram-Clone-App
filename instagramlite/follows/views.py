40556166e3531d5f601a32bd7c99a40d37ad62ac
DynastyElvis <kipkemoi.rono@student.moringaschool.com>
2022-06-11 22:25:12 +0300

    ft: added views configurations

--- instagramlite/follows/views.py
+++ instagramlite/follows/views.py
@@ -1,3 +1,9 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
-
-# Create your views here.
+from django.shortcuts import render,redirect
+from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse
+from .forms import UserRegisterForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
